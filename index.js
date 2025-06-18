42e31c4e5671d2c515873aad681954f7643b9c4b
Myitian <miaoyitian233@gmail.com>
2025-06-18 08:28:19 +0800

    fix bug

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ for (const tab of TABS) {
 function clickEvent(event) {
     /** @ts-ignore @type {HTMLButtonElement} */
     const self = event.target;
-    document.querySelector(`section[data-id="${self.dataset.id}"]`)?.scrollTo({ top: 0, behavior: "smooth" });
+    window.scrollTo({ top: 0, behavior: "smooth" });
 }
 
 function detectHashChange() {
@@ -47,11 +47,36 @@ function detectHashChange() {
                 page.classList.remove("active");
             }
         }
+    } else {
+        const element = document.getElementById(hash);
+        if (!element) {
+            return;
+        }
+        /** @type {HTMLElement} */
+        const ancestor = element.closest("section[data-id]");
+        if (!ancestor) {
+            return;
+        }
+        for (const tab of TABS) {
+            if (tab.dataset.id === ancestor.dataset.id) {
+                tab.classList.add("active");
+                anySelected = true;
+            } else {
+                tab.classList.remove("active");
+            }
+        }
+        for (const page of PAGES) {
+            if (page.dataset.id === ancestor.dataset.id) {
+                page.classList.add("active");
+                if (page.dataset.name) {
+                    document.title = page.dataset.name;
+                }
+                anySelected = true;
+            } else {
+                page.classList.remove("active");
+            }
+        }
     }
-}
-
-function scrollToTop() {
-    window.scrollTo({ top: 0, behavior: "smooth" });
 }
 
 function redirectToDonate() {
